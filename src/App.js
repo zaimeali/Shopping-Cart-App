1b91e93997d842973c777a1fb72441f5cdc50257
Zaime Ali <zaimeali1997@gmail.com>
2020-06-13 22:46:54 +0500

    UI Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// Custom Styling
 import './App.css';
+
+// Bootstrap
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// Images
+import love from './assets/logo/love.png';
+import shopping from './assets/logo/shopping.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="App-title">Shopping Cart <img className="App-title-image" src={ shopping } alt="Shopping Cart Logo" /></h2>
       </header>
+
+      <main className="App-main">
+      </main>
+
+      <footer className="App-footer">
+        <p>Made in <span className="App-footer-title">React</span> with <img className="App-footer-image" src={ love } alt="Heart Logo" /></p>
+      </footer>
     </div>
   );
 }
---
+++ src/data/MobileData.js
@@ -0,0 +1,66 @@
+export const data = [
+    { 
+        id: 1, 
+        name: 'Infinix Note 7', 
+        price: 26999, 
+        image: 'img/m1.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 2, 
+        name: 'Xiaomi Redmi Note 9 Pro', 
+        price: 44999, 
+        image: 'img/m2.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 3, 
+        name: 'Nokia 7.2', 
+        price: 47500, 
+        image: 'img/m3.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 4, 
+        name: 'Xiaomi Redmi Note 9S', 
+        price: 39999, 
+        image: 'img/m4.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 5, 
+        name: 'LG V30 Plus', 
+        price: 80999, 
+        image: 'img/m5.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 6, 
+        name: 'Apple iPhone XS Max', 
+        price: 169999,
+         image: 'img/m6.jpg',
+          inCart: false,
+           count:1
+         },
+    { 
+        id: 7, 
+        name: 'Sony Xperia X Premium', 
+        price: 75000, 
+        image: 'img/m7.jpg', 
+        inCart: false, 
+        count:1 
+    },
+    { 
+        id: 8, 
+        name: 'Motorola Moto Z Play', 
+        price: 43900, 
+        image: 'img/m8.jpg', 
+        inCart: false, 
+        count:1 
+    }
+]

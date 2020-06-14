00de4be43d41bfaeecad8e7db4e41bfad9f96acd
Zaime Ali <zaimeali1997@gmail.com>
2020-06-14 22:36:38 +0500

    Styling Updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,13 +18,13 @@ export default class Cart extends Component {
     render() {
         let nf = new Intl.NumberFormat(); 
         return (
-        <div className="col-md-12 col-lg-12 col-sm-12 mt-4">
+        <div className="col-md-12 col-lg-12 col-sm-12 mt-4 col-xs-12">
             <MobileConsumer>
                 {
                     (value) => {
                         if(value){
                             return(
-                                <div className="col-md-12 col-lg-12 col-sm-12">
+                                <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12">
                                     <div className="text-center">
                                         <h2 className="Cart-title">Your Cart</h2>
                                     </div>
@@ -32,19 +32,19 @@ export default class Cart extends Component {
                                         { value.cart.length > 0 ? (
                                             <div className="container text-center">
                                                 <div className="row mx-auto Cart-title-header">
-                                                    <div className="col-md-2 offset-md-1 offset-lg-1 col-lg-2">
+                                                    <div className="col-md-2 col-sm-2 col-xs-2 offset-sm-1 offset-md-1 offset-lg-1 col-lg-2">
                                                         <strong>Name </strong>
                                                     </div>
-                                                    <div className="col-md-2 col-lg-2">
+                                                    <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                         <strong>Price </strong>
                                                     </div>
-                                                    <div className="col-md-2 col-lg-2">
+                                                    <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                         <strong>Quantity </strong>
                                                     </div>
-                                                    <div className="col-md-2 col-lg-2">
+                                                    <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                         <strong>Remove</strong>
                                                     </div>
-                                                    <div className="col-md-2 col-lg-2">
+                                                    <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                         <strong>Total</strong>
                                                     </div>
                                                 </div>
@@ -52,27 +52,27 @@ export default class Cart extends Component {
                                                     value.cart.map(data => {
                                                         return(
                                                             <div className="row mx-auto card-show py-2">
-                                                                <div className="col-md-2 offset-md-1 offset-lg-1 col-lg-2">
+                                                                <div className="col-md-2 col-sm-2 col-xs-2 offset-sm-1 offset-md-1 offset-lg-1 col-lg-2">
                                                                     <span>{ data.name }</span>
                                                                 </div>
-                                                                <div className="col-md-2 col-lg-2 cart-price-style">
+                                                                <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2 cart-price-style">
                                                                     PKR <span className="cart-price">
                                                                             { nf.format(data.price) }
                                                                         </span>
                                                                 </div>
-                                                                <div className="col-md-2 col-lg-2">
+                                                                <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                                     <span>
                                                                         <input type="button" size="sm" value="-" className="minus btn-qty" onClick={ () => value.decrement(data.id) } />
                                                                         { data.count }
                                                                         <input type="button" size="sm" value="+" className="plus btn-qty" onClick={ () => value.increment(data.id) } />
                                                                     </span>
                                                                 </div>
-                                                                <div className="col-md-2 col-lg-2">
+                                                                <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                                     <Button className="removeBtn" size="sm" variant="outline-secondary" onClick={ () => value.removeMobile(data.id) }>
                                                                         Remove <span><img className="trash-delete" alt="Delete" src={ require(`../../assets/logo/trash.png`) } /></span>
                                                                     </Button>
                                                                 </div>
-                                                                <div className="col-md-2 col-lg-2">
+                                                                <div className="col-md-2 col-sm-2 col-xs-2 col-lg-2">
                                                                     <span>
                                                                         { nf.format(data.total) }
                                                                     </span>
